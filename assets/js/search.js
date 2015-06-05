aff306661f5c3118b87c6d419753ce306a0f13ab
Johnnie <johnnielo50@gmail.com>
2015-06-05 00:46:39 -0700

    Added a basic layout of the page. Used AJAX calls from search.js to call the WeatherConroller to access YQL module.

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,15 +1,15 @@
 function loadData() {
 	var location = $('#location').val(); 
 	var weatherUrl = '/weather?location=' + location; 
-
+	$("#form-container")[0].reset(); 
 	$.getJSON(weatherUrl, function(data) {
 		var resultObject = JSON.parse(data);	// location = {city, country, region}
 												// condition = {code, date, temp, text} 
 												// astronomy = {sunrise, sunset}
-		//$('#test').text(resultObject[0].city); 
 		$('#sunrise').text('Sunrise : ' + resultObject[2].sunrise);
-		$('#place').text('City : ' + resultObject[0].city + ' Region: ' + resultObject[0].region); 
-		$('#weather').text('Weather Condition : ' + resultObject[1].text + ' Temperature : ' + resultObject[1].temp); 
+		$('#place').text(resultObject[0].city + ' ' + resultObject[0].region); 
+		$('#weather').text('Weather Condition ' + resultObject[1].text); 
+		$('#temperature').text('Temperature ' + + resultObject[1].temp + ' F')
 		$('#sunset').text('Sunset : ' + resultObject[2].sunset); 
 
 	}).error(function(e) {
